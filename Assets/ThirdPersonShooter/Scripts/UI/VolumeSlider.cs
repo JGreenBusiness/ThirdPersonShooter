4b42976018dbfc97607acdbc387c7c1f0cbccbfb
Justin Green <s213902@students.aie.edu.au>
2022-12-07 16:15:28 +1100

    UI (UIScriptSetUP) Got up to making MainMenu

---
+++ Assets/ThirdPersonShooter/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using ThirdPersonShooter.Utility;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ThirdPersonShooter.UI
+{
+	[RequireComponent(typeof(Slider))]
+	public class VolumeSlider : MonoBehaviour
+	{
+		private const float MIN_VALUE = 0.001f;
+		private const float MAX_VALUE = 1f;
+
+		[SerializeField] private TextMeshProUGUI volumeText;
+
+		private Slider slider;
+		private string sliderName;
+		private string parameter;
+
+		private void Awake()
+		{
+			slider = gameObject.GetComponent<Slider>();
+		}
+
+		private void Activate()
+		{
+			parameter = GameManager.Instance.Settings[transform.GetSiblingIndex()];
+
+			slider.minValue = MIN_VALUE;
+			slider.maxValue = MAX_VALUE;
+
+			sliderName = parameter.Replace("Volume", "");
+
+			slider.value = PlayerPrefs.GetFloat(parameter, MAX_VALUE);
+			GameManager.Instance.Settings.SetVolume(parameter,slider.value);
+			UpdateText();
+			
+			slider.onValueChanged.AddListener(OnSliderValueChanged);
+		}
+
+		private void OnSliderValueChanged(float _value)
+		{
+			GameManager.Instance.Settings.SetVolume(parameter, slider.value);
+			UpdateText();
+		}
+
+		private void UpdateText()
+		{
+			volumeText.text = $"{sliderName}: {Mathf.RoundToInt(slider.value.Remap(MIN_VALUE, MAX_VALUE, 0, 100)):000}%";
+		}
+	}
+}

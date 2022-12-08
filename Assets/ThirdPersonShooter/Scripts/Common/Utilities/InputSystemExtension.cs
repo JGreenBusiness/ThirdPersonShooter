15e24fa9b614927e631238472f52257f804f93e1
Justin Green <s213902@students.aie.edu.au>
2022-12-08 16:18:32 +1100

    Enemy (Enemy&Shooting) : completed all enemy and shooting functins

---
+++ Assets/ThirdPersonShooter/Scripts/Common/Utilities/InputSystemExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ThirdPersonShooter.Utilities
+{
+	public static class InputSystemExtension
+	{
+		public static bool IsDown(this InputAction _action)
+		{
+			return Mathf.Approximately(_action.ReadValue<float>(), 1);
+		}
+	}
+}
--- Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
+++ Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
+using ThirdPersonShooter.UI;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonShooter.Entities.Player
 {
@@ -10,10 +13,56 @@ namespace ThirdPersonShooter.Entities.Player
 		public Vector3 Position => transform.position;
 
 		[SerializeField] private Stats stats;
+		[SerializeField] private InputActionReference pauseAction;
+
+		[Header("Components")] 
+		[SerializeField] private PlayerInput input;
+		[SerializeField] private Weapon weapon;
+		[SerializeField] private AudioSource hurtSource;
+		[SerializeField] private AudioSource deathSource;
+		
 
 		private void Awake()
 		{
 			stats.Start();
+			stats.onDeath += OnDeath;
+			stats.onHealthChanged += OnHealthChanged;
+			
+			weapon.SetPlayer(this);
+
+			if(GameManager.IsValid())
+			{
+				GameManager.Instance.Player = this;
+			}
+
+			if(UIManager.IsValid())
+			{
+				input.uiInputModule = UIManager.Instance.inputModule;
+			}
+			
+			
 		}
+
+		private void OnDestroy()
+		{
+			stats.onDeath -= OnDeath;
+			stats.onHealthChanged -= OnHealthChanged;
+		}
+
+		private void OnEnable() => pauseAction.action.performed += OnPausePerformed;
+
+		private void OnDisable() => pauseAction.action.performed -= OnPausePerformed;
+
+		private void OnPausePerformed(InputAction.CallbackContext _context)
+		{
+			if(GameManager.IsValid())
+			{
+				GameManager.Instance.TogglePaused();
+			}
+		}
+
+		private void OnDeath() => deathSource.Play();
+		private void OnHealthChanged(float _health) => hurtSource.Play();
+
 	}
 }
